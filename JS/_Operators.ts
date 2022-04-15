1b98dd5489231ca24a5faac29cf14e3a3799de73
Jesus Aguas <jesusaguasacin@gmail.com>
2022-04-15 22:06:58 +0200

    Added Operators

---
+++ JS/_Operators.ts
@@ -0,0 +1,55 @@
+/*
+JavaScript Arithmetic Operators:
++	    Addition
+-	    Subtraction
+*	    Multiplication
+**	    Exponentiation (ES2016)
+/	    Division
+%	    Modulus (Division Remainder)
+++	    Increment
+--	    Decrement
+
+JavaScript Assignment Operators:
+Op     Example     Same as
+=	    x = y	    x = y
++=	    x += y	    x = x + y
+-=	    x -= y	    x = x - y
+*=	    x *= y	    x = x * y
+/=	    x /= y	    x = x / y
+%=	    x %= y	    x = x % y
+**=	    x **= y	    x = x ** y
+
+JavaScript String Operators:
++   +=   add (concatenate) strings
+Adding two numbers, will return the sum, but adding a number and a string will return a string:
+
+JavaScript Comparison Operators:
+==	    equal to
+===	    equal value and equal type
+!=	    not equal
+!==	    not equal value or not equal type
+>	    greater than
+<	    less than
+>=	    greater than or equal to
+<=	    less than or equal to
+?	    ternary operator
+
+JavaScript Logical Operators:
+&&	    logical and
+||	    logical or
+!	    logical not
+
+JavaScript Bitwise Operators:
+Operator	Description     Example     Same as     Result      Decimal
+    &           AND          5 & 1    0101 & 0001	 0001       1
+    |	        OR	         5 | 1	  0101 | 0001	 0101	    5
+    ~	        NOT	         ~ 5	  ~0101	         1010	    10
+    ^	        XOR	         5 ^ 1	  0101 ^ 0001	 0100	    4
+    <<	    left shift	     5 << 1	  0101 << 1	     1010	    10
+    >>	    right shift	     5 >> 1	  0101 >> 1	     0010	    2
+ >>>	 unsigned right shift  5 >>> 1	0101 >>> 1	 0010	    2
+
+JavaScript Type Operators:
+typeof	        Returns the type of a variable
+instanceof	    Returns true if an object is an instance of an object type
+*/
