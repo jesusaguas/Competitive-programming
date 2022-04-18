4758531e4106877ef5edfb83438de365a7d36b62
Jesus Aguas <jesusaguasacin@gmail.com>
2022-04-19 00:05:06 +0200

    Added Types

--- Algorithms/Sorting/MergeSort.ts
+++ Algorithms/Sorting/MergeSort.ts
@@ -11,24 +11,23 @@ meaning it takes extra space other than the input array.
 This is because we are using auxiliary (helper) arrays to store the sub-arrays.
 One advantage of Merge Sort is that it lends itself very well to multi-threading, 
 since each respective half and be sorted on its own.
- 
+
 - Space complexity:         O(n)
 - Time compexity(worst):    O(nlogn)
 */
 
-function mergeSort(array) {
-    const half = array.length / 2
-    
+function mergeSort(array: number[]) {
     // Base case or terminating case
     if(array.length < 2){
       return array 
     }
-    
+
+    const half = array.length / 2
     const left = array.splice(0, half)
     return merge(mergeSort(left),mergeSort(array))
 }
 
-function merge(left, right) {
+function merge(left: number[], right: number[]) {
     let arr = []
     // Break out of loop if any one of the array gets empty
     while (left.length && right.length) {
