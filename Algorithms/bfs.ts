1339acec4280ad02b63ea8bac47e08abce71ddc1
Jesus Aguas <jesusaguasacin@gmail.com>
2022-04-26 23:03:39 +0200

    Added bfs

---
+++ Algorithms/bfs.ts
@@ -0,0 +1,33 @@
+var graph1: any = {
+    'A' : ['B','C'],
+    'B' : ['D', 'E', 'F'],
+    'C' : ['G'],
+    'D' : [],
+    'E' : [],
+    'F' : ['H'],
+    'G' : ['I'],
+    'H' : [],
+    'I' : []
+}
+
+function bfs(graph, node){
+    let visited = new Set();
+    let queue = [];
+
+    visited.add(node);
+    queue.push(node);
+
+    while (queue.length>0){
+        let s = queue.shift()
+        console.log(s)
+
+        for (let n of graph[s]){
+            if(!visited.has(n)){
+                visited.add(n)
+                queue.push(n)
+            }
+        }
+    }
+}
+
+bfs(graph1,'A')
